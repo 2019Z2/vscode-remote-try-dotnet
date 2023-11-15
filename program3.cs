0612aac09a7c493123001087573c29f91317b887
2019Z2 <79469120+2019Z2@users.noreply.github.com>
2023-11-15 11:11:34 +0000

    new file:   program3.cs, nov 11

---
+++ program3.cs
@@ -0,0 +1,22 @@
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int DAY = 1
+switch (DAY)
+{
+    case 1:
+        // Statements executed if expression(or variable) = value1
+        Console.WriteLine("NULL")
+        break;
+    case 2:
+        // Statements executed if expression(or variable) = value1
+        break;
+    /*
+    */
+    default:
+        // Statements executed if no case matches
+        Console.WriteLine("ERRDEF")
+}
+    }
+}
