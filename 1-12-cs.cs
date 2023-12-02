501f205653595ce10a880925cf84a1899ef112d1
2019Z2 <79469120+2019Z2@users.noreply.github.com>
2023-12-02 16:17:42 +0200

    Add files via upload
    extended lesson + homework

---
+++ 1-12-cs.cs
@@ -0,0 +1,19 @@
+
+
+int [] grades = {100, 80 , 90, 70};
+
+
+int Avg = Avarage(grades);
+
+int Avarage(int[] grades)
+{
+    int sum = 0;
+    int avg =0 ;
+    for(int i = 0; 1 < grades.Length; i++)
+    {
+        sum += grades{i}
+    }
+    avg = sum / 4;
+    return avg;
+
+}
---
+++ prg-tsk3-011223.cs
@@ -0,0 +1,17 @@
+using System;
+public class public ASSW
+{
+
+
+public static void Main (string[] args)
+{
+    int x = 12;
+    int y = 6;
+    int f = shlomke(x,y);
+    Console.WriteLine(f);
+}
+public static int Shlomke(int x, int y)
+{
+return x + y;
+}
+}
